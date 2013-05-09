14fe412e82f4ca22e70393f134d0e1126e7d3bea
Daniel Cheng <z@ithildin.com>
2013-05-09 11:44:29 -0700

    Rewrite buff tracker to understand worlds and be easier to configure.
    The original implementation mixed up the code and buff definitions,
    which made it hard to understand. It also had the unfortunate habit of
    /echo'ing messages to the foreground world instead of the world which
    triggered the message.
    
    The rewrite scopes buffs to the world they're defined on, and also
    provides some helper macros to quickly define when a buff starts and
    ends. As a bonus, all the helper triggers are now invisible by default,
    which makes /listdef much cleaner.

--- _tinyfugue/buff_tracker.tf
+++ _tinyfugue/buff_tracker.tf
@@ -1,24 +1,47 @@
 ; Buff duration tracking magic.
-; Generic timer helpers.
 /loaded buff_tracker.tf
 
 /require assoc.tf
 
-/def timestring = \
+/def -i _buff_tracker_timestring = \
   /let minutes $[(%{*}) / 60]%;\
   /let seconds $[mod(%{*}, 60)]%;\
-  /result strcat(%{minutes}, "m ", %{seconds}, "s") 
-/def start_timer = \
-  /assoc timer %{*}=$[trunc(time())]
-/def stop_timer = \
-  /let alpha $(/rassoc timer %{*})%;\
+  /result strcat(%{minutes}, "m ", %{seconds}, "s")
+
+/def -i _buff_tracker_start_timer = \
+  /assoc buff_tracker_time %{*}=$[trunc(time())]
+
+/def -i _buff_tracker_stop_timer = \
+  /let alpha $(/rassoc buff_tracker_time %{*})%;\
   /let omega $[trunc(time())]%;\
-  /assoc timer %{*}%;\
-  /echo -aBCmagenta $[strcat(">>> ", %{*}, " lasted for ", timestring(%{omega}-%{alpha}), " <<<")]
+  /assoc buff_tracker_time %{*}%;\
+  /echo -aBCmagenta -w$[world_info()] \
+    $[strcat(">>> ", $(/rassoc buff_tracker_id %{*}), " lasted for ",\
+             _buff_tracker_timestring(%{omega}-%{alpha}), " <<<")]
+
+; /ltimer
+; Lists all tracked buffs for the current world and the elapsed time.
 /def ltimer = \
-  /echo -aB ================================== Active Timers ==================================%;\
-  /mapassoc timer=/_ltimer%;\
-  /echo -aB ===================================================================================
-/def -i _ltimer = \
+  /let world $[world_info()]%;\
+  /echo -aB $[strcat(strrep("=", 32), " Active Timers ", strrep("=", 32))]%;\
+  /mapassoc buff_tracker_time %{world}=/_buff_tracker_ltimer_helper%;\
+  /echo -aB $[strrep("=", 79)]
+
+/def -i _buff_tracker_ltimer_helper = \
   /split %{*}%;\
-  /echo $[pad(strcat(%{P1}, ": "), -30, timestring(trunc(time()) - %{P2}), -49)]
+  /echo $[pad(strcat($(/rassoc buff_tracker_id %{P1}), ": "), -30,\
+              timestring(trunc(time()) - %{P2}), -49)]
+
+; /declare_buff <world> <identifier> <friendly string>
+/def declare_buff = \
+  /assoc buff_tracker_id $[strcat(%{1}, "_", %{2})]=%{-2}
+
+; /add_buff_start_message <world> <identifier> <message>
+/def add_buff_start_message = \
+  /def -i -w%{1} -msimple -t%{-2} = \
+    /_buff_tracker_start_timer $[strcat(%{1}, "_", %{2})]
+
+; /add_buff_end_message <world> <identifier> <message>
+/def add_buff_end_message = \
+  /def -i -w%{1} -msimple -t%{-2} = \
+    /_buff_tracker_stop_timer $[strcat(%{1}, "_", %{2})]
