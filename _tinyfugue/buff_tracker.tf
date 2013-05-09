e7ae884eadab910d8d3d038f8094762b8559e6b7
Daniel Cheng <z@ithildin.com>
2013-05-09 13:41:21 -0700

    Fix broken timestring calculation and broken buff name lookup.
    Timestring calculation was broken since /rassoc overrites %{P1} and
    %{P2}. Instead, capture them into local variables before invoking
    /rassoc.
    
    Buff name lookup has been fixed as well; /mapassoc returns the entire
    key name, which includes the table name, so the table name needs to be
    filtered out before looking up the buff name.

--- _tinyfugue/buff_tracker.tf
+++ _tinyfugue/buff_tracker.tf
@@ -3,9 +3,17 @@
 
 /require assoc.tf
 
-/def -i _buff_tracker_timestring = \
-  /let minutes $[(%{*}) / 60]%;\
-  /let seconds $[mod(%{*}, 60)]%;\
+; /_buff_tracker_get_second_word
+; Simple helper to extract the second word in a string.
+/def -i _buff_tracker_get_second_word = \
+  /echo %{2}
+
+; _buff_tracker_timediff(<start>, <end>)
+; Pretty prints the difference between two timestamps.
+/def -i _buff_tracker_timediff = \
+  /let difference $[%{2} - %{1}]%;\
+  /let minutes $[(%{difference}) / 60]%;\
+  /let seconds $[mod(%{difference}, 60)]%;\
   /result strcat(%{minutes}, "m ", %{seconds}, "s")
 
 /def -i _buff_tracker_start_timer = \
@@ -17,7 +25,7 @@
   /assoc buff_tracker_time %{*}%;\
   /echo -aBCmagenta -w$[world_info()] \
     $[strcat(">>> ", $(/rassoc buff_tracker_id %{*}), " lasted for ",\
-             _buff_tracker_timestring(%{omega}-%{alpha}), " <<<")]
+             _buff_tracker_timediff(%{alpha}, %{omega}), " <<<")]
 
 ; /ltimer
 ; Lists all tracked buffs for the current world and the elapsed time.
@@ -29,8 +37,11 @@
 
 /def -i _buff_tracker_ltimer_helper = \
   /split %{*}%;\
-  /echo $[pad(strcat($(/rassoc buff_tracker_id %{P1}), ": "), -30,\
-              timestring(trunc(time()) - %{P2}), -49)]
+  /let key_name %{P1}%;\
+  /let start_time %{P2}%;\
+  /let buff_id $(/_buff_tracker_get_second_word %{key_name})%;\
+  /echo $[pad(strcat($(/rassoc buff_tracker_id %{buff_id}), ": "), -30,\
+              _buff_tracker_timediff(%{start_time}, trunc(time())), -49)]
 
 ; /declare_buff <world> <identifier> <friendly string>
 /def declare_buff = \
